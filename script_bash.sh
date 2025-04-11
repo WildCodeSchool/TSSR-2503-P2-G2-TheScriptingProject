30dd7a69f9a05c60caa3f1ff99b677d75b821be1
Florian <floriancheron.2002@gmail.com>
2025-04-11 10:24:12 +0200

    Correctif 1.2 script_bash.sh

--- script_bash.sh
+++ script_bash.sh
@@ -1,120 +1,6 @@
 #!/bin/bash
 
-
-
-# ====>   ScriptBash Sprint1
-# Squelette global
-
-# Faire des logs a chaques actions pour garder une trace => journalisation log_evt.log
-# Pour le serveur windows, dans C:\Windows\System32\LogFiles
-# Pour le serveur Linux, dans /var/log
-# Sous la forme ---> <Date>-<Heure>-<Utilisateur>-<Evenement>
-# Avec :
-# <Date> : Date de l’evenement au format yyyy/mm/dd
-# <Heure> : Heure de l'événement au format hh/mm/ss
-# <Utilisateur> : Nom de l’utilisateur courant exécutant le script
-# <Evenement> : Action effectuée (à définir )
-
-# Deux Type de choix globaux ===> Action ou Information
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Création compte utilisateur local 
-# - Changement de mot de passe 
-# - Suppression compte utilisateur local
-# - Désactivation compte utilisateur local 
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Ajout à un groupe local 
-# - Sortie d'un groupe local 
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Arret 
-# - Redémarrage 
-# - Vérouillage 
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Mise-à-jour du système 
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Création de répertoire 
-# - Modification de repertoire 
-# - -Suppression de répertoire 
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Prise en main a distance (CLI)
-
-# Fonction : ========> "Fait ou non fait"
-
-# - Définition de règle de pare-feu 
-# - Activation du pare-feu
-# - Désactivation du pare feu 
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Type de CPU, nombre de coeurs, etc.
-# - Mémoire RAM totale
-# - Utilisation de la RAM
-# - Utilisation du disque
-# - Utilisation du processeur
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Date de dernière connexion d’un utilisateur
-# - Date de dernière modification du mot de passe
-# - Liste des sessions ouvertes par l'utilisateur
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Groupe d’appartenance d’un utilisateur
-# - Historique des commandes exécutées par l'utilisateur
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Droits/permissions de l’utilisateur sur un dossier
-# - Droits/permissions de l’utilisateur sur un fichier
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Version de l'OS
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Nombre de disque
-# - Partition (nombre, nom, FS, taille) par disque
-
-#Fonction : ========> "Fait ou non fait"
-
-# - Liste des applications/paquets installées
-# - Liste des services en cours d'execution
-# - Liste des utilisateurs locaux
-
-
-
-#Fonction : 
-
-# - Recherche des evenements dans le fichier log_evt.log pour un utilisateur
-# - Recherche des evenements dans le fichier log_evt.log pour un ordinateur
-
-#Fonction : 
-
-# - Mise-à-jour du système 
-
-#Fonction 
-
-# - Installation de logiciel
-# - Désinstallation de logiciel 
-# - Execution de script sur la machine distante 
-
-
-###################################################
-
-######################## JOURNALISATION ##############################
+####################### JOURNALISATION ##############################
 # On veut obtenir les informations sur la machine et l'utilisateur actuel
 # On veut enregistrer les informations dans un dossier "log"
 enregistrement_information() { 
@@ -131,13 +17,6 @@ enregistrement_information() {
     # On ajuste le nom du fichier pour une information utilisateur sous la forme <NomDuPC>_<NomDeLUtilisateur>_<Date>.txt     
         user_info_log="info_$(hostname)_$(whoami)_$date.txt"
 }
-
-
-
-
-
-
-
 
 
 
